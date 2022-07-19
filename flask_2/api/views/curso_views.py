798051d9a4d29c811a2cb7058fefcfb05d46a5c8
willanderdev <willanderdev@gmail.com>
2022-07-19 19:08:43 -0300

    commit inicial

---
+++ flask_2/api/views/curso_views.py
@@ -0,0 +1,11 @@
+from flask_restful import Resource
+
+from api import api
+
+
+class CursoList(Resource):
+    def get(self):
+        return "Estudando API com FLaskdfd"
+
+
+api.add_resource(CursoList, '/cursos')
